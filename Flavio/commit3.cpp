412bb86f3b77a43c60335419c91305f35de9ffe9
Flaviohmm <fhenrique609@gmail.com>
2014-10-25 20:48:39 -0200

    Create commit3.cpp

---
+++ Flavio/commit3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{   ///{ Write you code here
+
+    float a,b,c;
+
+    cout<<endl;
+    cin>>a;
+    cout<<endl;
+    cin>>b;
+    cout<<endl;
+    cin>>c;
+
+
+    ///}
+    cout<<"("<<a<<" + "<<b<<" + "<<c<<")/2 is:"<<(a+b+c)/2;
+}
