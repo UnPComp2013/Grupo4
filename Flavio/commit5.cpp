674724e3ee0b27eb960ddbdd890edc1fb7139fbe
Flaviohmm <fhenrique609@gmail.com>
2014-10-25 21:05:47 -0200

    Create commit5.cpp

---
+++ Flavio/commit5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+   float a,x,b,c=0;
+   cout<<"\nEnter 1st number:";
+   cin>>a;
+   cout<<"\nEnter 2nd number:";
+   cin>>b;
+   cout<<"\nEnter 3rd number:";
+   cin>>x;
+
+
+   ///{ Write your code here
+
+   c=a+b+x;
+
+   ///}
+
+
+   cout<<c;
+}
